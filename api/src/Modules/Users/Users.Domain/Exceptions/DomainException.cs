4f56326445afe3d1ec4865ad466dda798063d8bb
JoaoVitorAguiar <joaovitor4guiar@gmail.com>
2025-03-16 17:28:38 -0300

    feat: add DomainException for handling domain-specific exceptions

---
+++ api/src/Modules/Users/Users.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,7 @@
+namespace Users.Domain.Exceptions;
+
+public abstract class DomainException(string title, int statusCode, object? details = null) : Exception(title)
+{
+    public int StatusCode { get; } = statusCode;
+    public object? Details { get; } = details;
+}
