76953db0af4ef137f81ced5a8cf3c58d20bfa730
JoaoVitorAguiar <joaovitor4guiar@gmail.com>
2025-03-16 10:43:40 -0300

    fix: update inequality operator to handle nullable Entity parameters

--- api/src/Shared/Entities/Entity.cs
+++ api/src/Shared/Entities/Entity.cs
@@ -53,7 +53,7 @@ public abstract class Entity
         return a.Equals(b);
     }
 
-    public static bool operator !=(Entity a, Entity b)
+    public static bool operator !=(Entity? a, Entity? b)
     {
         return !(a == b);
     }
