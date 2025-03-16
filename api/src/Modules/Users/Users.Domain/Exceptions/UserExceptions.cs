c2dbd8a0575064c3f8104126d43372ca15fbe447
JoaoVitorAguiar <joaovitor4guiar@gmail.com>
2025-03-16 17:29:41 -0300

    feat: add UserExceptions

---
+++ api/src/Modules/Users/Users.Domain/Exceptions/UserExceptions.cs
@@ -0,0 +1,8 @@
+namespace Users.Domain.Exceptions;
+
+public class UserAlreadyExistsException(string email)
+    : DomainException($"User already exists", 409, details: $"A user with the email '{email}' is already registered." );
+    
+public class UserNotFoundException(string id)
+    : DomainException($"A user with the ID '{id}' was not found", 404);
+    
