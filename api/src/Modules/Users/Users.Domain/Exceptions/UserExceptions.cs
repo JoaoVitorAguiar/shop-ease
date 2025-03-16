37c5302a12c5d4b4cdc4f0d6fd9ba9f68adc0ebb
JoaoVitorAguiar <joaovitor4guiar@gmail.com>
2025-03-16 17:46:12 -0300

    feat: add InvalidCredentialsException

--- api/src/Modules/Users/Users.Domain/Exceptions/UserExceptions.cs
+++ api/src/Modules/Users/Users.Domain/Exceptions/UserExceptions.cs
@@ -6,3 +6,9 @@ public class UserAlreadyExistsException(string email)
 public class UserNotFoundException(string id)
     : DomainException($"A user with the ID '{id}' was not found", 404);
     
+public class InvalidCredentialsException()
+    : DomainException(
+        title: "Invalid credentials", 
+        statusCode: 401, 
+        details: "The provided email or password is incorrect" 
+    );
